6362625f5d387bd8c48cec4c12216e9542d4996f
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2023-06-01 15:33:34 -0300

    req 1 feito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    raise NotImplementedError
+    if not all(
+        isinstance(period[0], int) and isinstance(period[1], int)
+        for period in permanence_period
+    ):
+        return None
+
+    if not target_time:
+        return None
+
+    count_students = 0
+
+    for period in permanence_period:
+        if target_time >= period[0] and target_time <= period[1]:
+            count_students += 1
+
+    return count_students
