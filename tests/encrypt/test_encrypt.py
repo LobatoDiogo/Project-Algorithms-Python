825bd945d7786815f1824c4bf01cae46022c27e5
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2023-06-01 18:09:16 -0300

    tests

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    expected = encrypt_message("testes", 3)
+    assert expected == "set_set"
+
+    expected = encrypt_message("testes", 4)
+    assert expected == "se_tset"
+
+    expected = encrypt_message("testes", 9)
+    assert expected == "setset"
+
+    with pytest.raises(TypeError):
+        encrypt_message(1, 1)
+
+    with pytest.raises(TypeError):
+        encrypt_message("erro", "error")
