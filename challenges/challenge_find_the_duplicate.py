f85221867e20983905fa2de598ffd5363d857790
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2023-06-01 17:24:36 -0300

    req 5 feito

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,15 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (
+        nums is None
+        or len(nums) <= 1
+        or any(not isinstance(num, int) or num < 0 for num in nums)
+    ):
+        return False
+
+    nums.sort()
+
+    for index in range(1, len(nums)):
+        if nums[index] == nums[index - 1]:
+            return nums[index]
+
+    return False
