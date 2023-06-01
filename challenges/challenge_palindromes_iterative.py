a07d21929b324098d463a2688cc41f334e6dbca9
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2023-06-01 16:01:04 -0300

    palindromes challenges feito

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,11 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    word_size = len(word)
+
+    for index in range(word_size // 2):
+        if word[index] != word[word_size - index - 1]:
+            return False
+
+    return True
